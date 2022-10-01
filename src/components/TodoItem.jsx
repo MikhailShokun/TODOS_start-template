d599fa70916d55205eaafd312a37edac154d7bbf
MikhailShokun <minishokun@gmail.com>
2022-10-01 18:57:18 +0300

    +++

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {useDispatch} from "react-redux";
-import {removeTodo, toggleTodoCompleted} from "../app/todoSlice";
+import { useDispatch } from "react-redux";
+import { removeTodo, toggleTodoCompleted } from "../app/todoSlice";
 
-function TodoItem({id, text, completed}) {
-    const dispatch = useDispatch();
+function TodoItem({ id, text, completed }) {
+  const dispatch = useDispatch();
 
-    return (
-        <li key={id}>
-            <input type="checkbox" checked={completed} onChange={() => dispatch(toggleTodoCompleted({id}))}/>
-            <span>{text}</span>
-            <span onClick={() => dispatch(removeTodo({id}))} className="delete">
+  return (
+    <li>
+      <input type="checkbox" checked={completed} onChange={() => dispatch(toggleTodoCompleted({ id }))} />
+      <span>{text}</span>
+      <span onClick={() => dispatch(removeTodo({ id }))} className="delete">
         &times;
       </span>
-        </li>
-    );
+    </li>
+  );
 }
 
 export default TodoItem;
