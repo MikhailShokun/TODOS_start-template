499b931bef291d5614a48c9fd9c41bd546323f20
MikhailShokun <minishokun@gmail.com>
2022-10-04 13:51:37 +0300

    added typing

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
     return (
         <div className={"App"}>
-            <h1>Todo 01</h1>
+            <h1>Todo 01 withAsyncThunk</h1>
 
             < InputField title={title}
                          setText={setText}
